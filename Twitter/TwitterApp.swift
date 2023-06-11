c48b1924538cd38cf6199d76d31b64c5e0186d9a
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-06-11 14:18:35 -0400

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by user242230 on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
